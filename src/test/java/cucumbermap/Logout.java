6398044a44a043f11c9a754a20e6e5368e8b7002
ganesh gandal <ganeshgandal@gmail.com>
2021-08-12 08:19:41 +0530

    By Mr.Jack

---
+++ src/test/java/cucumbermap/Logout.java
@@ -0,0 +1,21 @@
+package cucumbermap;
+
+public class Logout 
+{
+	
+	public void log()
+	{
+		System.out.println("12323");
+	}
+	
+	
+	
+	public static void main(String[] args)
+	{
+		Logout obj=new Logout();
+		
+		   obj.log();
+		
+	}
+
+}
